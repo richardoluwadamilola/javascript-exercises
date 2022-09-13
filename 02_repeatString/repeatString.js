5f2c02cd8b00c7f3cb2b6eecfe87a5f48d373238
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-09-13 10:41:06 +0100

    some tests passed, some failed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(Hi, times) {
+    if (times < 0) return 'error';
+    let string = '';
+    for (let i = 0; i < times; i++) {
+        string += Hi;
+    }
+    return string;
 };
 
 // Do not edit below this line
